8b7a6145f730131b5bd199c67eed715795196981
janneadolf <janne.adolf@posteo.de>
2021-01-21 14:52:58 +0100

    Add documentation to myFun()

--- R/f_test.R
+++ R/f_test.R
@@ -1,8 +1,23 @@
-#' The first test function
+#' Run a simple linear model y on x
+#'
+#' The function takes x and y, binds them into data frame, fits a linear model and returns the fit object.
+#'
+#' More details here.
+#'
+#' @param x a numeric vector treated as the predictor in the linear model
+#' @param y a numeric vector treated as the outcome in the linear model
+#' @param ... currently disregarded
+#'
+#' @return the fit object from the lm function
+#'
+#' @examples
+#' x <- 1:10
+#' y <- 4 + .5*x + rnorm(10, 0, 1)
+#' myFun(x, y)
 #'
 #' @importFrom stats lm
 #' @export
-myFun <- function(x,y){
+myFun <- function(x,y,...){
   tmp <- data.frame(x,y)
   out <- lm(y ~x, data = tmp)
   return(out)
